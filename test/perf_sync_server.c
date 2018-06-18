bdbe3f07c0464db81126c0cb509533f0a168a587
lazebird <lazebird@gmail.com>
2018-06-18 01:41:02 -0400

    more

---
+++ test/perf_sync_server.c
@@ -0,0 +1,25 @@
+#include "hello.h"
+#include <crpc.h>
+#include <stdio.h>
+#include <string.h>
+
+msg_reply_t *hello(msg_req_t *msg)
+{
+  static msg_reply_t res;
+//   printf("req [%d] %s\n", msg->id, msg->info);
+  memset(&res, 0, sizeof(res));
+  res.id = msg->id;
+  snprintf(res.info, sizeof(res.info), "%s %s", "hello", msg->info);
+  snprintf(res.server, sizeof(res.server), "%s", HELLO_SERVER_PATH);
+  return &res;
+}
+
+int main(int argc, char *argv[])
+{
+//   crpc_debug(0x3);
+  crpc_srv_init(HELLO_SERVER_PATH);
+  CRPC_API_REG(hello);
+  crpc_srv_run(-1);
+  crpc_srv_destroy();
+  return 0;
+}
